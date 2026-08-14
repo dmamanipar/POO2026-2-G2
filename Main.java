18bd2ffcd08d044179c525bff8a088b82b141cfe
dmamanipar <128348888+dmamanipar@users.noreply.github.com>
2026-08-14 11:32:17 -0500

    Create Main.java
    Ejemplo

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+
+    public static void imprimeMatriz(int[][] m){
+      for (int f=0;f<m.length;f++){
+        for (int c=0;c<m[0].length;c++ ) {
+          System.out.print(m[f][c]+"\t");
+        }
+        System.out.println("");
+      } 
+    }
+
+    public static int[][] rellenarMf5(int tm, int numI){
+      int [][] m=new int[tm][tm];
+      for (int f=0;f< m.length;f++ ){//3
+        for (int c=m[0].length-1; c>=m[0].length-1-f;c--){//1 c =3
+            m[f][c]=numI;
+            numI++;
+        } 
+      } 
+      return m;
+    }
+
+    public static void main(String[] args) {
+      //int [][] a={ {1,2},{3,2}, {4,2} };
+      imprimeMatriz(rellenarMf5(5,0));
+    }
+
+}
