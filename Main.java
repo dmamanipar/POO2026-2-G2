e7761785226c1993a694d1a5bb89da890dbbab4e
dmamanipar <128348888+dmamanipar@users.noreply.github.com>
2026-08-14 12:36:43 -0500

    Update Main.java
    Cambio Varios

--- Main.java
+++ Main.java
@@ -36,10 +36,59 @@ public class Main {
       return m;
     }
 
+    public static int[][] rellenarMf7(int tm, int numI){
+      int [][] m=new int[tm][tm];
+       for (int c=0;c<m.length ;c++){
+          for (int f=0;f<=c;f++){
+            m[f][c]=numI;
+            numI++;
+          } 
+       } 
+      return m;
+    }
+    public static int[][] rellenarMf17(int tm, int numI){
+      int [][] m=new int[tm][tm];
+       for (int c=m[0].length-1;c>=0;c--){
+          for (int f=0;f<=c;f++){
+            m[f][c]=numI;
+            numI++;
+          } 
+       } 
+      return m;
+    }
+    public static int[][]rellenarMf9(int tm, int numI){
+      int [][] m=new int [tm][tm];
+      for (int c=0;c<m.length ;c++){
+        for(int f=0;f<=c;f++){
+          m[f][c]=numI;
+            numI++;
+        }
+      }
+      return m;
+    }
+  public static int[][] rellenarMf16(int tm, int numI){
+      int [][] m=new int[tm][tm];
+      for (int c=0;c< m.length;c++ ){
+        for (int f=m[0].length-1; f>=m[0].length-1-c;f--){
+            m[f][c]=numI;
+            numI++;
+        } 
+      } 
+      return m;
+    }
     public static void main(String[] args) {
       //int [][] a={ {1,2},{3,2}, {4,2} };
       imprimeMatriz(rellenarMf5(5,1));
-      System.out.print("");
+      System.out.println("");
       imprimeMatriz(rellenarMf6(5,1));
+      System.out.println("");
+      imprimeMatriz(rellenarMf7(5,1));
+      System.out.println("");
+      imprimeMatriz(rellenarMf17(5,1));
+      System.out.println("");
+      imprimeMatriz(rellenarMf9(5,1));
+        System.out.println("");
+      System.out.println("");
+      imprimeMatriz(rellenarMf16(5,1));
     }
 }
