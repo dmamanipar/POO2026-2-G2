a2a0d8041691b6e500e4d954f8639f318383168f
dmamanipar <dmamanipar@unsa.edu.pe>
2026-08-28 12:21:46 -0500

    Limpiado repositorio 1

--- AlgotimosGUI/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module pe.edu.upeu.algotimosgui {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens pe.edu.upeu.algotimosgui to javafx.fxml;
-    exports pe.edu.upeu.algotimosgui;
-}
