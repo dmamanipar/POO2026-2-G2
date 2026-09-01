cc0fb56293e6e6f074b256b4c1141c768048120b
dmamanipar <dmamanipar@unsa.edu.pe>
2026-09-01 17:04:22 -0500

    generado

---
+++ AlgotimosGUI/src/main/java/pe/edu/upeu/algotimosgui/Launcher.java
@@ -0,0 +1,9 @@
+package pe.edu.upeu.algotimosgui;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+       Application.launch(MatrizGUI.class, args);
+    }
+}
