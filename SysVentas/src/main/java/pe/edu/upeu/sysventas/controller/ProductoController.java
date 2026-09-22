dd4bc0dfbfb989e59172f374f5bf922a7b073ea2
dmamanipar <dmamanipar@unsa.edu.pe>
2026-09-22 14:04:17 -0500

    avance p1

--- SysVentas/src/main/java/pe/edu/upeu/sysventas/controller/ProductoController.java
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/controller/ProductoController.java
@@ -1,6 +1,15 @@
 package pe.edu.upeu.sysventas.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import lombok.RequiredArgsConstructor;
+import pe.edu.upeu.sysventas.dto.ComboBoxOption;
+import pe.edu.upeu.sysventas.model.Producto;
 import pe.edu.upeu.sysventas.service.ICategoriaService;
 import pe.edu.upeu.sysventas.service.IMarcaService;
 import pe.edu.upeu.sysventas.service.IProductoService;
@@ -12,4 +21,23 @@ public class ProductoController {
     private final ICategoriaService cs;
     private final IProductoService ps;
     private final IUnidadMedidaService us;
+
+    @FXML
+    TextField txtNombreProducto, txtPUnit,
+            txtPUnitOld, txtUtilidad, txtStock, txtStockOld, txtFiltroDato;
+    @FXML
+    ComboBox<ComboBoxOption> cbxTipoProducto;
+    @FXML ComboBox<ComboBoxOption> cbxMarca;
+    @FXML ComboBox<ComboBoxOption> cbxCategoria;
+    @FXML ComboBox<ComboBoxOption> cbxUnidMedida;
+
+    @FXML private TableView<Producto> tableView;
+
+    @FXML
+    Label lbnMsg;
+    @FXML private AnchorPane miContenedor;
+    Stage stage;
+
+
+
 }
